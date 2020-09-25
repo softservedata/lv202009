a92f98023523221d32ada6cf357ed41e2c60fa36
marianahuzar <maryana.huzar@gmail.com>
2020-09-25 21:13:03 +0300

    added homework2 with actions

---
+++ src/com/softserve/edu/hw2/Appl.java
@@ -0,0 +1,15 @@
+package com.softserve.edu.hw2;
+
+public class Appl {
+    public static void main(String[] args) {
+        int k = 16;
+        System.out.println(k++);
+        System.out.println(++k);
+        System.out.println(k+++k);
+        System.out.println(50-(--k));
+        System.out.println(k*k);
+        System.out.println(k/3);
+        System.out.println(k%3);
+        System.out.println(k++<++k);
+    }
+}
