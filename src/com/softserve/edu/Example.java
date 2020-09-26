13f2e037ba78509b6bdabc28caee92b9fe60a9f7
Dmytro Harus <garus.dv@gmail.com>
2020-09-26 20:09:30 +0300

    changes in class Example

--- src/com/softserve/edu/Example.java
+++ src/com/softserve/edu/Example.java
@@ -2,9 +2,12 @@ package com.softserve.edu;
 
 public class Example {
     public static void main(String[] args) {
-        System.out.println("My first program");
-        for (int i = 0; i < args.length; i++) {
-            System.out.println("Arg " + i + " is " + args[i]);
+        System.out.println("My first program is loop 'for'");
+        for (int i = 0; i < 5; i++) {
+            if (i==3)
+            System.out.println("i= " + i);
+            else
+                System.out.println("hidden");
         }
     }
 }
