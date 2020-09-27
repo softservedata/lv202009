944569a88120341b181b1f036441b25525dced95
nshpo <shporlyuk.n@gmail.com>
2020-09-27 17:36:16 +0300

    add Appl

---
+++ src/com/softserve/edu/hw2/Appl.java
@@ -0,0 +1,41 @@
+package com.softserve.edu.hw2;
+
+public class Appl {
+
+	public static void main(String[] args) {
+	
+	 int k = 16;
+	 
+     System.out.println(k++); 
+	 // 16 is printed, then k becomes 17
+	 
+     /*
+     System.out.println(++k);
+	 // k becomes 17 then "17" is printed
+	 
+     System.out.println(k+++k);
+     // 16 + 17 = 33 then "33" is printed
+	 
+	 int b=50;
+     System.out.println(b---k);
+     // to resolve 50---k, prints 34
+	 
+	 System.out.println(k*k);
+     // 16 * 16 = 256 then "256" is printed
+	 
+	 
+	 System.out.println(k/3);
+	 // 16/3=5.3 and remove fraction ".3", then 5 is printed 
+	
+	 
+	 System.out.println(k%3);
+	 // 16/3 is 15/3=5 and "1" remaining 
+	 
+	
+	System.out.println(k++<++k);
+	// 16 < 17 = true is printed
+	 */
+       
+    }
+
+}
