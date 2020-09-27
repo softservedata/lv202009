322f8cbc4f43580ecf729690ae8b178846b8cd49
Daria Koshelieva <dashka@Darias-MacBook-Pro.local>
2020-09-28 00:40:20 +0300

    hw2

---
+++ src/com/softserve/edu/Appl.java
@@ -0,0 +1,16 @@
+package com.softserve.edu;
+
+public class Appl {
+     public static void main(String[] args) {
+            int k = 16;
+            int m = 50;
+            System.out.println(k++);
+            System.out.println(++k);
+            System.out.println(k+++k);
+            System.out.println(m---k);
+            System.out.println(k*k);
+            System.out.println(k/3);
+            System.out.println(k%3);
+            System.out.println(k++<++k);
+     }
+}
