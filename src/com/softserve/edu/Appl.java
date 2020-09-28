d2ccd2bd38442cf40c6921416d48ba27830a1c44
ss-svetatsehm <ssdd2006@ukr.net>
2020-09-28 09:12:14 +0300

    Add code - 1st HW - Sveta Tsehmeistruk

---
+++ src/com/softserve/edu/Appl.java
@@ -0,0 +1,11 @@
+package com.softserve.edu;
+
+public class Appl {
+    public static void main(String[] args) {
+        System.out.println("");
+        int k = 16;
+        k++;
+        /* ++k; k+++k; 50---k; k*k; k/3; k%3; k++<++k; */
+        System.out.println("Result is " + k);
+    }
+}
