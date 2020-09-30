0461765314784f469551fd33e3cd1e296e7453c9
Dasha Koshelieva <dasha.koshelieva@gmail.com>
2020-09-30 22:40:38 +0300

    add comments

--- src/com/softserve/edu/Appl.java
+++ src/com/softserve/edu/Appl.java
@@ -4,13 +4,13 @@ public class Appl {
      public static void main(String[] args) {
             int k = 16;
             int m = 50;
-            System.out.println(k++);
-            System.out.println(++k);
-            System.out.println(k+++k);
-            System.out.println(m---k);
+            System.out.println(k++);  // Increment after print
+            System.out.println(++k);  // Increment before print
+            System.out.println(k+++k); // Similar to k++ + k
+            System.out.println(m---k); // m - k and decrement m after
             System.out.println(k*k);
             System.out.println(k/3);
             System.out.println(k%3);
-            System.out.println(k++<++k);
+            System.out.println(k++<++k);  // True because increment after always < increment now
      }
 }
