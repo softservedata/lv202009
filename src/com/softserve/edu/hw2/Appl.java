76e93c31e6bba8fd346cd93d867ca364500825f3
ss-svetatsehm <ssdd2006@ukr.net>
2020-09-30 21:07:26 +0300

    Add 2nd HW

---
+++ src/com/softserve/edu/hw2/Appl.java
@@ -0,0 +1,31 @@
+package com.softserve.edu.hw2;
+
+	public class Appl {
+
+	    /**
+	     * Method <font color="red">Main</font>
+	     * @param args from command line (hints for me)
+	     */
+	    public static void main(String[] args) {
+	        System.out.println("");
+	        int k = 16;
+	        k++;
+	        // ++k; k+++k; 50---k; k*k; k/3; k%3; k++<++k;
+	        System.out.println("Result k++ is " + k);
+
+	        ++k;
+	        System.out.println("Result ++k is " + k);
+	        
+	        //50---k;
+	        //System.out.println("Result 50---k is " + k);
+	        
+	        //k*k;
+	        //System.out.println("Result k*k is " + k);
+	        
+	        //k/3;
+	        //System.out.println("Result k/3 is " + k);
+	        
+	        //k++<++k;
+	        //System.out.println("Result k++<++k is " + k);
+	    }
+	}
