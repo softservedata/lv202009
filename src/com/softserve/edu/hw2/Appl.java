09d535298b145a85ed75667d3decd500c42c3b9b
tdmytr <tetyanadmytryshyn@gmail.com>
2020-10-02 17:21:12 +0300

    HW 2

---
+++ src/com/softserve/edu/hw2/Appl.java
@@ -0,0 +1,32 @@
+package com.softserve.edu.hw2;
+
+public class Appl {
+    public static void main(String[] args){
+        int k = 16;
+        //System.out.println(k++);
+        // k=16
+
+        //System.out.println(++k);
+        // K=18
+
+        //System.out.println(k+++k);
+        // k=37
+
+        //System.out.println(50-(--k));
+        // k= 32
+
+        //System.out.println(k*k);
+        // k=324
+
+        //System.out.println(k/3);
+        // k=5
+
+        //System.out.println(k%3);
+        // k=1
+
+        System.out.println(k++<++k);
+        // true
+
+    }
+
+}
