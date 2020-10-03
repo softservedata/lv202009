a0e8982bf9ba00aa7f7bf835cf8d955e5ea7d4f6
svitlana_firsova <sfirso@softserveinc.com>
2020-10-03 20:52:32 +0300

    changed hw3

--- src/com/softserve/edu/hw3/Homework3.java
+++ src/com/softserve/edu/hw3/Homework3.java
@@ -5,28 +5,44 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Homework3 {
-	  public static void main(String[] args) throws IOException {
-		  
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
 
-	System.out.println("Hello. What is your name?");
-	String name = br.readLine();
-	System.out.println("How old are you?");
-	int age = Integer.parseInt(br.readLine());	
-	System.out.println("Do you have pet?");
-	String pet = br.readLine();
-	if (pet.equalsIgnoreCase("yes")){
-		System.out.println("What is it name");
-		String petName = br.readLine();
-		System.out.println("Hello " + name + "!");  
-		System.out.println("You are " + age + " years old");
-		System.out.println("You have pet and it's name is " + petName);
-	} else
-	{System.out.println("Goodbye ");}
-	  }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("Hello. What is your name?");
+		String name = br.readLine();
+		System.out.println("How old are you?");
+		int age = Integer.parseInt(br.readLine());
+		System.out.println("Do you have pet?");
+		String pet = br.readLine();
+		if (pet.equalsIgnoreCase("yes")) {
+			System.out.println("What is it name");
+			String petName = br.readLine();
+			System.out.println("Hello " + name + "!");
+			System.out.println("You are " + age + " years old");
+			System.out.println("You have pet and it's name is " + petName);
+		} else {
+			System.out.println("Goodbye ");
+		}
+
+		System.out.println("Enter first numbers and press Enter btn");
+		int a = Integer.parseInt(br.readLine());
+		System.out.println("Enter second numbers and press Enter btn");
+		int b = Integer.parseInt(br.readLine());
+		System.out.println("Enter third numbers and press Enter btn");
+		int c = Integer.parseInt(br.readLine());
+		if (a < b & b < c) {
+			System.out.println("ASC order: " + a + " " + b + " " + c);
+		} else if (a < c & c < b) {
+			System.out.println("ASC order: " + a + " " + c + " " + b);
+		} else if (b < a & a < c) {
+			System.out.println("ASC order: " + b + " " + a + " " + c);
+		} else if (b < c & c < a) {
+			System.out.println("ASC order: " + b + " " + c + " " + a);
+		} else if (c < a & a < b) {
+			System.out.println("ASC order: " + c + " " + a + " " + b);
+		} else if (c < b & b > a) {
+			System.out.println("ASC order: " + c + " " + b + " " + a);
+		}
 	}
-
-	
-	
-
-
+}
---
+++ src/com/softserve/edu/hw4/Product.java
@@ -0,0 +1,5 @@
+package com.softserve.edu.hw4;
+
+public class Product {
+
+}
