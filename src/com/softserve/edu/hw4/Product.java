a4e6741929cd12dcbecadf816be92b4842463411
marianahuzar <maryana.huzar@gmail.com>
2020-10-04 22:03:34 +0300

    added homework4 which shows the most expensive product

---
+++ src/com/softserve/edu/hw4/Product.java
@@ -0,0 +1,68 @@
+package com.softserve.edu.hw4;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Product {
+
+    private String productName;
+    private float productPrice;
+
+    public Product() {
+    }
+
+    public Product(String productName, float productPrice) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public float getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(float productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Product " +
+                "productName='" + productName + '\'' +
+                ", productPrice=" + productPrice;
+    }
+
+    public static void main(String[] args) {
+        Product prod1 = new Product("Bag", 12.5f);
+        Product prod2 = new Product("Backpack", 30);
+        Product prod3 = new Product();
+        prod3.setProductName("Purse");
+        prod3.setProductPrice(128);
+        Product prod4 = new Product();
+        prod4.setProductName("Wallet");
+        prod4.setProductPrice(18.25f);
+
+        Product mostExpensiveProduct = prod1;
+        if (mostExpensiveProduct.getProductPrice() < prod2.getProductPrice()) {
+            mostExpensiveProduct = prod2;
+        }
+        if (mostExpensiveProduct.getProductPrice() < prod3.getProductPrice()) {
+            mostExpensiveProduct = prod3;
+        }
+        if (mostExpensiveProduct.getProductPrice() < prod4.getProductPrice()) {
+            mostExpensiveProduct = prod4;
+        }
+
+        System.out.println("Most expensive product is: "
+                + mostExpensiveProduct.getProductName());
+    }
+
+
+}
