bebe4c57843e20daabd6c9d9fc005d3bd06a53cd
ss-svetatsehm <ssdd2006@ukr.net>
2020-10-04 18:23:20 +0300

    Add HW 3

---
+++ src/com/softserve/edu/hw3/Hw3.java
@@ -0,0 +1,38 @@
+package com.softserve.edu.hw3;
+
+import javax.xml.soap.SOAPPart;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Hw3 {
+    public static void main(String[] args) throws IOException {
+
+        // ввід з клавіатури
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введіть число №1:");
+        String num1 = reader.readLine();
+        System.out.println("Введіть число №2:");
+        String num2 = reader.readLine();
+        System.out.println("Введіть число №3:");
+        String num3 = reader.readLine();
+
+        // стрoка -> число
+        int n1 = Integer.parseInt(num1);
+        int n2 = Integer.parseInt(num2);
+        int n3 = Integer.parseInt(num3);
+
+        if (n1 >= n2 && n2 >= n3) System.out.print("\n" + n3 + " " + n2 + " " + n1);
+        else if (n1 >= n3 && n3 >= n2) System.out.print("\n" + n2 + " " + n3 + " " + n1);
+        else if (n2 >= n1 && n1 >= n3) System.out.print("\n" + n3 + " " + n1 + " " + n2);
+        else if (n2 >= n3 && n3 >= n1) System.out.print("\n" + n1 + " " + n3 + " " + n2);
+        else if (n3 >= n1 && n1 >= n2) System.out.print("\n" + n2 + " " + n1 + " " + n3);
+        else if (n3 >= n2 && n2 >= n1)System.out.print("\n" + n1 + " " + n2 + " " + n3);
+    }
+}
+
+//    Прочитати з клавіатури три числа (цілих або дійсних).
+//    Вивести ці числа на екран у порядку зростання.
+//    Для коду використати пакет com.softserve.edu.hw3
+//    Закомітити та запушати код на github у свою вітку.
