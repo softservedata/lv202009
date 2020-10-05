4ab06124bc7bfa92b6101a3e4ac3300c9aa155b5
ss-svetatsehm <ssdd2006@ukr.net>
2020-10-05 09:26:05 +0300

    Added HW3 - edited

--- src/com/softserve/edu/hw3/Hw3.java
+++ src/com/softserve/edu/hw3/Hw3.java
@@ -4,10 +4,16 @@ import javax.xml.soap.SOAPPart;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Hw3 {
     public static void main(String[] args) throws IOException {
+
+        //    Прочитати з клавіатури три числа (цілих або дійсних).
+        //    Вивести ці числа на екран у порядку зростання.
+        //    Для коду використати пакет com.softserve.edu.hw3
+        //    Закомітити та запушати код на github у свою вітку.
 
         // ввід з клавіатури
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -23,16 +29,15 @@ public class Hw3 {
         int n2 = Integer.parseInt(num2);
         int n3 = Integer.parseInt(num3);
 
-        if (n1 >= n2 && n2 >= n3) System.out.print("\n" + n3 + " " + n2 + " " + n1);
-        else if (n1 >= n3 && n3 >= n2) System.out.print("\n" + n2 + " " + n3 + " " + n1);
-        else if (n2 >= n1 && n1 >= n3) System.out.print("\n" + n3 + " " + n1 + " " + n2);
-        else if (n2 >= n3 && n3 >= n1) System.out.print("\n" + n1 + " " + n3 + " " + n2);
-        else if (n3 >= n1 && n1 >= n2) System.out.print("\n" + n2 + " " + n1 + " " + n3);
-        else if (n3 >= n2 && n2 >= n1)System.out.print("\n" + n1 + " " + n2 + " " + n3);
+//        if (n1 >= n2 && n2 >= n3) System.out.print("\n" + n3 + " " + n2 + " " + n1);
+//        else if (n1 >= n3 && n3 >= n2) System.out.print("\n" + n2 + " " + n3 + " " + n1);
+//        else if (n2 >= n1 && n1 >= n3) System.out.print("\n" + n3 + " " + n1 + " " + n2);
+//        else if (n2 >= n3 && n3 >= n1) System.out.print("\n" + n1 + " " + n3 + " " + n2);
+//        else if (n3 >= n1 && n1 >= n2) System.out.print("\n" + n2 + " " + n1 + " " + n3);
+//        else if (n3 >= n2 && n2 >= n1)System.out.print("\n" + n1 + " " + n2 + " " + n3);
+
+        int[] array = new int[] {n1, n2, n3};
+        Arrays.sort(array);
+        System.out.println(array[array.length - 1]);
     }
 }
-
-//    Прочитати з клавіатури три числа (цілих або дійсних).
-//    Вивести ці числа на екран у порядку зростання.
-//    Для коду використати пакет com.softserve.edu.hw3
-//    Закомітити та запушати код на github у свою вітку.
