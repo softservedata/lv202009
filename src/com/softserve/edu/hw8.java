34064a2d69cd6aab8c71e6e168c10b010fc69c59
ss-svetatsehm <ssdd2006@ukr.net>
2020-11-02 09:55:43 +0200

    Added hw8

--- src/com/softserve/edu/hw8.java
+++ src/com/softserve/edu/hw8.java
@@ -1,5 +1,8 @@
 package com.softserve.edu;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class hw8 {
     /*-----------------------------------------*/
     // Task 8
@@ -10,6 +13,45 @@ Homework 8 (from 22.10.2020)
 Для коду використати пакет com.softserve.homework8
  */
     public static void main(String[] args) {
+        // Collection (List)
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
 
+        list1.add(1);
+        list1.add(2);
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(2, 200);
+
+        list2.add(1);
+        list2.add(3);
+        list2.add(1);
+        list2.add(2);
+        list2.add(1);
+        list2.add(1);
+
+        System.out.println("Array1: " + list1);
+        System.out.println("Array2: " + list2);
+
+//        Collections.sort(list1);
+//        Collections.sort(list2);
+//
+//        System.out.println("Array1 sorted: " + list1);
+//        System.out.println("Array2 sorted: " + list2);
+
+        ArrayList<Integer> result = commonElements(list1, list2);
+        System.out.println("Common elements are: \n" + result);
+    }
+
+    public static ArrayList<Integer> commonElements(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        ArrayList<Integer> resultList = new ArrayList<>();
+        for (Integer number : list1) {
+            if (list2.contains(number)) {
+                resultList.add(number);
+                list2.remove(number);
+            }
+        }
+        return resultList;
     }
 }
