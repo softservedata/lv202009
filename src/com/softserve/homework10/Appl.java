57aba361cd61edc8a6fc0942a2f4c1e38eab663b
marianahuzar <maryana.huzar@gmail.com>
2020-11-04 21:37:16 +0200

    added homework10

---
+++ src/com/softserve/homework10/Appl.java
@@ -0,0 +1,49 @@
+package com.softserve.homework10;
+
+import javax.xml.soap.Text;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Appl {
+
+    public static void main(String[] args) {
+        String text = "His \\)next class (is art). (]He [draws] on {paper} with {crayons} and pencils and [sometimes] uses a ruler. \n" +
+                "Lucas (likes) art. It \\[{((is))} his [favorite class]. His \\(third [class] {is} science.";
+
+        boolean result = verifyBrackets(text);
+        System.out.println(result);
+    }
+
+    public static boolean verifyBrackets(String text) {
+        String result = text.replaceAll("(\\\\\\))|(\\\\\\()|(\\\\\\])|(\\\\\\[)|(\\\\\\{)|(\\\\\\})", "")
+                .replaceAll("[^\\(\\)\\{\\]\\}\\[]", "");
+        System.out.println(result);
+
+        if (result.isEmpty()) {
+            return true;
+        } else {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < result.length(); i++) {
+                char current = result.charAt(i);
+                if (current == '(' || current == '[' || current == '{') {
+                    stack.push(current);
+                } else {
+                    if (stack.isEmpty()) {
+                        return false;
+                    }
+                    char peekChar = stack.peek();
+                    if ((current == ')' && peekChar != '(')
+                            || (current == '}' && peekChar != '{')
+                            || (current == ']' && peekChar != '[')) {
+                        return false;
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+            return true;
+        }
+
+    }
+}
