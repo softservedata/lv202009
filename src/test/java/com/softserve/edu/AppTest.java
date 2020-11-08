970947e9b3e3e04a0b0d10d89b1ec0f8c2d3a926
svitlana_firsova <sfirso@softserveinc.com>
2020-11-08 20:48:25 +0200

    add mvn test

---
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -0,0 +1,17 @@
+package com.softserve.edu;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+//import org.junit.Assert;
+//import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        Assert.assertTrue(true);
+        System.out.println("class AppTest @Test testApp() done");
+    }
+    
+}
