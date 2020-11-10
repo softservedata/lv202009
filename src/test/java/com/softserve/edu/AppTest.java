ba345345ea333785d2d0d293d2d50ada061e76c9
ss-svetatsehm <ssdd2006@ukr.net>
2020-11-10 12:30:51 +0200

    Add Junit project to mvn branch

---
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -0,0 +1,18 @@
+package com.softserve.edu;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    @Test
+    public void testApp()
+    {
+        Assert.assertTrue( true );
+        System.out.println("class AppTest @Test testApp() done");
+    }
+}
