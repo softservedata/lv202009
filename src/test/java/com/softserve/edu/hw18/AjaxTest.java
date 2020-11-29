96199838f715c6685995f86918486ff8be6ca2b4
nshpo <shporlyuk.n@gmail.com>
2020-11-29 22:17:36 +0200

    hw18 AjaxTest

--- src/test/java/com/softserve/edu/hw18/AjaxTest.java
+++ src/test/java/com/softserve/edu/hw18/AjaxTest.java
@@ -1,11 +1,14 @@
-package com.softserve.edu17;
+package com.softserve.edu.hw18;
 
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -15,17 +18,18 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Login2Test {
-	private final String BASE_URL = "http://taqc-opencart.epizy.com/";
+public class AjaxTest {
+
+	private final String BASE_URL = "https://www.w3schools.com/js/js_ajax_intro.asp";
 	private final Long IMPLICITLY_WAIT_SECONDS = 10L;
 	private final Long ONE_SECOND_DELAY = 1000L;
 	private WebDriver driver;
-	
+
 	private void presentationSleep() {
 		presentationSleep(1);
 	}
-	
-	//Overload
+
+	// Overload
 	private void presentationSleep(int seconds) {
 		try {
 			Thread.sleep(seconds * ONE_SECOND_DELAY);
@@ -33,50 +37,54 @@ public class Login2Test {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@BeforeSuite
 	public void beforeSuite() {
 		WebDriverManager.chromedriver().setup();
 	}
-	
+
 	@BeforeClass
 	public void beforeClass() {
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(IMPLICITLY_WAIT_SECONDS, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		
+
 	}
-	
-	@AfterClass(alwaysRun = true) 
+
+	@AfterClass(alwaysRun = true)
 	public void afterClass() {
 		presentationSleep();
 		driver.quit();
 	}
-	
+
 	@BeforeMethod
 	public void beforeMethod() {
 		driver.get(BASE_URL);
 		presentationSleep();
 	}
-	@AfterMethod
-    public void afterMethod() {
-        presentationSleep(); // For Presentation ONLY
-      
-    }
-	
-	@Test
-	public void loginPage() {
-		
-		driver.findElement(By.cssSelector(".list-inline > .dropdown")).click();
-		driver.findElement(By.cssSelector("ul > li.dropdown.open > ul > li:nth-child(2) > a")).click();
-		//
-		driver.findElement(By.cssSelector("#input-email")).click();
-		driver.findElement(By.cssSelector("#input-email")).sendKeys("test@gmail.com");
-		
 
-		Assert.assertTrue(driver.getTitle().contains("Account Login"));
-		
+	@AfterMethod
+	public void afterMethod() {
+		presentationSleep(); // For Presentation ONLY
+
 	}
-	
+
+	@Test
+	public void AjaxIframePage() {
+
+		WebElement position = driver.findElement(By.xpath("//div[@id='demo']/h2"));
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView(true);", position);
+
+		WebElement changeText = driver.findElement(By.cssSelector("button[onclick='loadDoc()']"));
+		changeText.click();
+		// wait until element disappears and find new text
+		if ((new WebDriverWait(driver, 10)).until(ExpectedConditions.stalenessOf(position))) {
+			(new WebDriverWait(driver, 10))
+					.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='demo']/p[2]")));
+		}
+
+	}
 	
 }
