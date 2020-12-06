2e194933e8c270e8cf9f5aa5ea6e4176f89672f8
ikov <omenon1995@gmail.com>
2020-12-06 17:32:02 +0200

    added ikov hm13

---
+++ src/main/java/com/softserve/hw13/AppBrackets.java
@@ -0,0 +1,17 @@
+package com.softserve.hw13;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AppBrackets {
+    public static void main(String[] args) {
+        Brackets brackets = new Brackets();
+        List<String> lst = Arrays.asList(
+                "(\\[)", "() [\\}] {}", "{(())\\(}", "(\\ ()",
+                "(]", ") (", "([)] ");
+        for (String current : lst) {
+            System.out.println("\nCurrent = " + current 
+                    + "  result = " + brackets.verifyBrackets(current));
+        }
+    }
+}
